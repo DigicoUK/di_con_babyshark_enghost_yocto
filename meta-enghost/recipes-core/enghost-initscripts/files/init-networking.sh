4e0b318c68877040242bb40572c6c6f46ea7ecd4
Liam Pribis <liam.pribis@digiconsoles.com>
2025-01-28 12:53:27 +0000

    read mac address from fash

--- meta-enghost/recipes-core/enghost-initscripts/files/init-networking.sh
+++ meta-enghost/recipes-core/enghost-initscripts/files/init-networking.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-ip addr add 192.168.1.5/24 dev eth0
-ip link set eth0 up
+INTERFACE="eth0"
+
+MAC_FILE="/immutable/macaddress"
+if [ -f "$MAC_FILE" ]; then
+    ip link set dev "$INTERFACE" address "$(cat /immutable/macaddress)"
+else
+    echo "$MAC_FILE does not exist"
+fi
+
+ip addr add 192.168.1.5/24 dev "$INTERFACE"
+ip link set "$INTERFACE" up
 
