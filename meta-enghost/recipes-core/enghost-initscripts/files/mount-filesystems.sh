e9913ff5d0741a9096f6e734a56ec0c38f948c87
Liam Pribis <liam.pribis@digiconsoles.com>
2025-01-28 11:45:36 +0000

    Mount flash and eMMC modules

--- meta-enghost/recipes-core/enghost-initscripts/files/mount-filesystems.sh
+++ meta-enghost/recipes-core/enghost-initscripts/files/mount-filesystems.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# first create the mountpoints
+mkdir -p /config
+mkdir -p /data
+mkdir -p /dante
+mkdir -p /enginefirmware
+mkdir -p /systempersist
+mkdir -p /immutable
+mkdir -p /engineparams
+
 mount -at nonfs,nosmbfs,noncpfs 2>/dev/null
